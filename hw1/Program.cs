1a64be4dd4c2cae6af227c244cee25de6d3156f5
«Ekaterina» <katuxa-muxa@mail.ru>
2022-12-21 21:20:03 +0200

    задачки решены, 3яя с ошибкой

---
+++ hw1/Program.cs
@@ -0,0 +1,106 @@
+﻿/*
+
+Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+
+void PrintArray(int[,] matr)
+        {
+            for (int i = 0; i < matr.GetLength(0); i++)
+            {
+                for (int j = 0; j < matr.GetLength(1); j++)
+                {
+                    Console.Write($"{matr[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        void FillArray(int[,] matr)
+        {
+            for (int i = 0; i < matr.GetLength(0); i++)
+            {
+                for (int j = 0; j < matr.GetLength(1); j++)
+                {
+                    matr[i, j] = new Random().Next(1, 100);
+                }
+            }
+        }
+
+
+void PrintArraySecond(int[,] matr1)
+        {
+            for (int i = 0; i < matr1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matr1.GetLength(1); j++)
+                {
+                    Console.Write($"{matr1[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        void FillArraySecond(int[,] matr1)
+        {
+            for (int i = 0; i < matr1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matr1.GetLength(1); j++)
+                {
+                    matr1[i, j] = new Random().Next(1, 100);
+                }
+            }
+        }
+
+//  перемножение
+
+
+
+void ResultArray(int[,] matr)
+{
+    
+
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+         
+         matr[i, j] = 0;
+
+        for (int k = 0; k < matr.GetLength(1); k++)
+        {
+            matr[i, j] = matr[i, j] + matr[i, k] * matr[k, j];
+        }
+    }
+}
+
+}
+
+
+
+
+        Console.WriteLine($"Задайте количество строк двумерного массива:");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Задайте количество столбцов двумерного массива:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[,] matrix = new int[m, n];
+        FillArray(matrix);
+       Console.WriteLine();
+        PrintArray(matrix);
+
+       Console.WriteLine();
+
+        FillArraySecond(matrix);
+        Console.WriteLine();
+        PrintArraySecond(matrix);
+        
+        Console.WriteLine();
+        ResultArray(matrix);
+        PrintArray(matrix);
+     
+        
---
+++ hw3/Program.cs
@@ -0,0 +1,64 @@
+﻿/* Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+    void PrintArray(int[,] matr)
+        {
+            for (int i = 0; i < matr.GetLength(0); i++)
+            {
+                for (int j = 0; j < matr.GetLength(1); j++)
+                {
+                    Console.Write($"{matr[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        void FillArray(int[,] matr)
+        {
+            for (int i = 0; i < matr.GetLength(0); i++)
+            {
+                for (int j = 0; j < matr.GetLength(1); j++)
+                {
+                    matr[i, j] = new Random().Next(1, 100);
+                }
+            }
+        }
+
+void NumberRowMinSumElements(int[,] array)
+{
+     int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+     for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minRow = minRow + array[0, i];
+    }
+         for (int i = 0; i < array.GetLength(0); i++)
+        {
+             for (int j = 0; j < array.GetLength(1); j++) sumRow = sumRow + array[i, j];
+             if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($" строка с наименьшей суммой = {minSumRow + 1}");
+}
+
+        Console.WriteLine($"Задайте количество строк двумерного массива:");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Задайте количество столбцов двумерного массива:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[,] matrix = new int[m, n];
+        FillArray(matrix);
+        Console.WriteLine(matrix);
+        PrintArray(matrix);
+        NumberRowMinSumElements(matrix);
+        
